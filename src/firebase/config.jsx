986e205d605c4a2c667184e17f0d50d5869ee5b4
Jerônimo Barbieri Junior <jeronimo.barbieri@fatec.sp.gov.br>
2025-03-11 14:44:40 -0300

    Configuração das Variaveis do Firebase

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
 import { getFirestore } from "firebase/firestore"
 
+
 const firebaseConfig = {
     apiKey: "AIzaSyDcbipwYEbwT1vx-4T4JXkrLwVwGH9KhDk",
     authDomain: "lifedev-romano.firebaseapp.com",
@@ -10,7 +11,7 @@ const firebaseConfig = {
     messagingSenderId: "1091586505139",
     appId: "1:1091586505139:web:cc3055782e77fe1a0133c6",
     measurementId: "G-R9FNWR61EW"
-};
+}
 
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
