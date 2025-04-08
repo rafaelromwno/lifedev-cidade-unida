92c3a32ec0af735ae1ed33b5f2d2ee9a78313284
Jeronimo Barbieri Junior <jeronimo.barbieri@fatec.sp.gov.br>
2025-04-08 11:02:41 -0300

    Entrega variaveis de comunicação do projeto com Firebase

---
+++ src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react"
+import {db} from "../firebase/config"
+import { collet, query, ordyBy, onSnapshot, where } from "firebase/firestore"
+
+export const useFetchDocuments = () =>{
+
+    
+}
---
+++ src/hooks/useUpdateDocument.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect, useReducer } from "react"
+import {db} from "../firebase/config"
+import {doc, updateDoc} from "firebase/firestore"
+
+export const useUpdateDocument = () =>{
+
+    
+}
