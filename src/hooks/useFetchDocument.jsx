92d85fdad7de2214e7f313d9c9fe1466e9c4e5d3
VictorHugoTesti <victortesti06@gmail.com>
2025-04-08 11:04:24 -0300

    Entrega variaveis de comunicação de projeto com Firebase

---
+++ src/hooks/useFetchDocument.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react"
+import { db } from "../firebase/config"
+import {doc, updateDoc} from "firebase/firestore"
+
+export const useFetchDocument = () => {
+
+}
---
+++ src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react"
+import { db } from "../firebase/config"
+import {
+    collect,
+    query,
+    orderBt,
+    onSnappbot,
+    where
+} from "firebase/firestore"
+
+
+export const useFetchDocuments = () => {
+
+}
