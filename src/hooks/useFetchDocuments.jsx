b59e5990ae60868637145d60d308b25222772667
rafaelromwno <romanosilvarafael@gmail.com>
2025-04-08 11:14:56 -0300

    Entrega das variáveis de Comunicação de projeto com Firebase

---
+++ src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  where,
+} from "firebase/firestore";
+
+export const useFetchDocuments = () => {
+  
+}
---
+++ src/hooks/useUpdateDocument.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import { updateDoc, doc } from "firebase/firestore";
+
+export const useUpdateDocument = () => {
+  
+}
