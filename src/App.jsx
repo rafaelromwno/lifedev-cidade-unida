dc6994a9d4095175d4ef1d7b62f092063f51bf86
rafaelromwno <romanosilvarafael@gmail.com>
2025-05-13 11:47:04 -0300

    Parte 1 - Aula 1305 - Firebase + Firestore

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,45 @@ import Footer from './components/Footer'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import { AuthProvider } from './context/AuthContext'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useEffect, useState } from 'react'
+import { useAuthentication } from './hooks/useAuthetication'
 
 function App() {
 
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication
+
+  const loadingUser = user == undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+  if(loadingUser) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <>
       <div>
-        <BrowserRouter>
-          <Navbar />
-          <div className='container'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-      </div>
+        <AuthProvider value={user}>
+            <BrowserRouter>
+              <Navbar />
+              <div className='container'>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<Register />} />
+                </Routes>
+              </div>
+              <Footer />
+            </BrowserRouter>
+          </AuthProvider>
+        </div>        
     </>
   )
 }
